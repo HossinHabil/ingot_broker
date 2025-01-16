2b124a769a95b65448f35e91d4ef50f0eeb61685
HossinHabil <husseinhabil92@gmail.com>
2025-01-16 19:43:21 +0200

    improve code 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "INGOT",
-  description: "INGOT Assessment",
+  title: "INGOT BROKERS",
+  description: "INGOT Brokers Assessment",
 };
 
 export default function RootLayout({
